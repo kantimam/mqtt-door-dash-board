3666b8da840690fe310928f524e5cf3bc8cf5392
Nyannazz <kantemir.imam@gmail.com>
2020-09-23 23:52:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, createContext, useReducer } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+
+
+const initialState = {
+  user: null
+}
+
+export const UserContext = createContext(initialState)
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case 'logIn':
+      return { ...state, user: action.payload }
+    case 'logOut':
+      return { ...state, user: null }
+
+    default:
+      throw new Error('Not among actions')
+  }
+}
 
 function App() {
+  const [user, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[user, dispatch]}>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <ProtectedRoute path="/" user={user}>
+            <Home />
+          </ProtectedRoute>
+        </Switch>
+      </div>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@material-ui/core'
+import React from 'react'
+
+const Home = (props) => {
+    console.log(props)
+    return (
+        <Box>
+            home
+        </Box>
+    )
+}
+
+export default Home
